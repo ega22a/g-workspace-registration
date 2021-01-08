a9e2a70a264b95912ea9a83acccf6146079167a5
Egor Repiev <repev.egor@nttek.ru>
2021-01-09 00:05:23 +0500

    Обновление исходного кода, добавление шаблонов писем.

--- exec.py
+++ exec.py
@@ -13,12 +13,14 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.utils import formataddr
 from email.header import Header
+from string import Template
 # Импорт форм
 from form_mainForm import Ui_mainForm
 from form_dialogJSONCreds import Ui_formJSONCreds
 from form_alert import Ui_formAlert
 from form_emailSettings import Ui_form_emailSettings
 from form_checkEmail import Ui_form_checkEmail
+from form_editUser import Ui_form_editUser
 
 # Инициализация констант
 SCOPES = [
@@ -39,6 +41,7 @@ _dialogAlert = None
 _dialogJSON = None
 _emailSettings = None
 _checkEmail = None
+_dialogEditUser = None
 
 # Рекурсивная функция создания организационных подразделений
 def collectOrgUnits(_orgUnits = None, _qTree = None):
@@ -59,8 +62,7 @@ def collectOrgUnits(_orgUnits = None, _qTree = None):
     return _returned
 
 # Функция проверки существования файла (относительные пути)
-def checkFileExsist(_file:str):
-    return os.path.isfile(os.path.join(EXEC_DIRECTORY, _file))
+checkFileExsist = lambda _file: os.path.isfile(os.path.join(EXEC_DIRECTORY, _file))
 
 # Инициализация подключения к Directory API (Google Workspace)
 def directoryAPI_exec():
@@ -81,6 +83,14 @@ def directoryAPI_exec():
     else:
         return False
 
+# Функция подстановки текста в шаблоны
+def getFormattedText(_filename:str = None, _args:dict = None):
+    if (_filename != None):
+        with open(_filename, 'r') as _f:
+            return _f.read() if _args == None else Template(_f.read()).substitute(_args)
+    else:
+        return False
+
 # Функция отправки писем через TLS SMTP
 def sendMail(_payloads:list = None):
     if (PAYLOAD.get('settings') != None) & (_payloads != None):
@@ -93,25 +103,7 @@ def sendMail(_payloads:list = None):
                 _message['Subject'] = _unit.get("subject")
                 _message['From'] = formataddr((str(Header(PAYLOAD.get('settings').get('names').get('from'), 'utf-8')), _email.get('login')))
                 _message['To'] = _unit.get('to')
-                _message.attach(MIMEText("""
-                    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;font-family:arial, 'helvetica neue', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"><head><meta charset="UTF-8"><meta content="width=device-width, initial-scale=1" name="viewport"><meta name="x-apple-disable-message-reformatting"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta content="telephone=no" name="format-detection"><title>Новый шаблон</title> <!--[if (mso 16)]><style type="text/css">     a {text-decoration: none;}     </style><![endif]--> <!--[if gte mso 9]><style>sup { font-size: 100% !important; }</style><![endif]--> <!--[if gte mso 9]><xml> <o:OfficeDocumentSettings> <o:AllowPNG></o:AllowPNG> <o:PixelsPerInch>96</o:PixelsPerInch>
-                    </o:OfficeDocumentSettings> </xml><![endif]--><style type="text/css">
-                    #outlook a {	padding:0;}.ExternalClass {	width:100%;}.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div {	line-height:100%;}.es-button {	mso-style-priority:100!important;	text-decoration:none!important;}a[x-apple-data-detectors] {	color:inherit!important;	text-decoration:none!important;	font-size:inherit!important;	font-family:inherit!important;	font-weight:inherit!important;	line-height:inherit!important;}.es-desk-hidden {	display:none;	float:left;	overflow:hidden;	width:0;	max-height:0;	line-height:0;	mso-hide:all;}@media only screen and (max-width:600px) {p, ul li, ol li, a { font-size:16px!important; line-height:150%!important } h1 { font-size:30px!important; text-align:center; line-height:120% } h2 { font-size:26px!important; text-align:center; line-height:120% } h3 { font-size:20px!important; text-align:center; line-height:120% } h1 a { font-size:30px!important } h2 a { 
-                    font-size:26px!important } h3 a { font-size:20px!important } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } .es-btn-fw { border-width:10px 0px!important; 
-                    text-align:center!important } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:600px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } 
-                    table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } a.es-button, button.es-button { font-size:20px!important; display:block!important; border-width:10px 0px 10px 0px!important } }</style></head><body style="width:100%;font-family:arial, 'helvetica neue', helvetica, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"><div class="es-wrapper-color" style="background-color:#F6F6F6"> <!--[if gte mso 9]><v:background xmlns:v="urn:schemas-microsoft-com:vml" fill="t"> <v:fill type="tile" color="#f6f6f6"></v:fill> </v:background><![endif]-->
-                    <table class="es-wrapper" width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;padding:0;Margin:0;width:100%;height:100%;background-repeat:repeat;background-position:center top"><tr style="border-collapse:collapse"><td valign="top" style="padding:0;Margin:0"><table cellpadding="0" cellspacing="0" class="es-header" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top"><tr style="border-collapse:collapse"><td align="center" style="padding:0;Margin:0"><table bgcolor="#ffffff" class="es-header-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px">
-                    <tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px"> <!--[if mso]><table style="width:560px" cellpadding="0" cellspacing="0"><tr><td style="width:270px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" class="es-left" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left"><tr style="border-collapse:collapse"><td class="es-m-p0r es-m-p20b" valign="top" align="center" style="padding:0;Margin:0;width:270px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td align="center" style="padding:0;Margin:0;font-size:0px">
-                    <img class="adapt-img" src="https://okptmc.stripocdn.email/content/guids/CABINET_f17bb2de294d5c72d027ebc0479224d8/images/10531609771525397.png" alt style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic" width="270"></td></tr></table></td></tr></table> <!--[if mso]></td><td style="width:20px"></td><td style="width:270px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0;width:270px"><table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td align="center" style="padding:0;Margin:0;display:none"></td></tr></table></td></tr></table> <!--[if mso]></td></tr></table>
-                    <![endif]--></td></tr></table></td></tr></table><table class="es-content" cellspacing="0" cellpadding="0" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%"><tr style="border-collapse:collapse"><td align="center" style="padding:0;Margin:0"><table class="es-content-body" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px" cellspacing="0" cellpadding="0" bgcolor="#ffffff" align="center"><tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0;padding-top:20px;padding-left:20px;padding-right:20px"><table width="100%" cellspacing="0" cellpadding="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse">
-                    <td class="es-m-p0r es-m-p20b" valign="top" align="center" style="padding:0;Margin:0;width:560px"><table width="100%" cellspacing="0" cellpadding="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td align="center" style="padding:20px;Margin:0;font-size:0"><table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td style="padding:0;Margin:0;border-bottom:1px solid #CCCCCC;background:none;height:1px;width:100%;margin:0px"></td></tr></table></td></tr><tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0">
-                    <p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:14px;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:21px;color:#333333">""" + _unit.get('message') + """</p></td></tr><tr style="border-collapse:collapse"><td align="center" style="padding:20px;Margin:0;font-size:0"><table border="0" width="100%" height="100%" cellpadding="0" cellspacing="0" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td style="padding:0;Margin:0;border-bottom:1px solid #CCCCCC;background:none;height:1px;width:100%;margin:0px"></td></tr></table></td></tr></table></td></tr></table></td></tr></table></td></tr></table>
-                    <table cellpadding="0" cellspacing="0" class="es-footer" align="center" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;table-layout:fixed !important;width:100%;background-color:transparent;background-repeat:repeat;background-position:center top"><tr style="border-collapse:collapse"><td align="center" style="padding:0;Margin:0"><table bgcolor="#ffffff" class="es-footer-body" align="center" cellpadding="0" cellspacing="0" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;background-color:#FFFFFF;width:600px"><tr style="border-collapse:collapse"><td align="left" style="Margin:0;padding-top:20px;padding-bottom:20px;padding-left:20px;padding-right:20px"> <!--[if mso]><table style="width:560px" cellpadding="0" cellspacing="0"><tr><td style="width:270px" valign="top"><![endif]-->
-                    <table cellpadding="0" cellspacing="0" class="es-left" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left"><tr style="border-collapse:collapse"><td class="es-m-p20b" align="left" style="padding:0;Margin:0;width:270px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:10px;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:15px;color:#333333">ГАПОУ СО "Нижнетагильский торгово-экономический колледж"&nbsp;© """ + str(datetime.now().year) + """ г.<br>Google LLC&nbsp;© """ + str(datetime.now().year) + """ г.</p></td></tr></table></td></tr></table> <!--[if mso]></td><td style="width:20px"></td>
-                    <td style="width:270px" valign="top"><![endif]--><table cellpadding="0" cellspacing="0" class="es-right" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:right"><tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0;width:270px"><table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px"><tr style="border-collapse:collapse"><td align="left" style="padding:0;Margin:0"><p style="Margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-size:10px;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:15px;color:#333333">622001, Россия, Свердловская область, г. Нижний Тагил, пр. Ленина, д. 2а</p></td></tr></table></td></tr></table> <!--[if mso]></td></tr></table><![endif]--></td></tr></table>
-                    </td></tr></table></td></tr></table></div></body>
-                    </html>
-                """, _subtype='html'))
+                _message.attach(MIMEText(getFormattedText('email/template.mail', {'mainbody': _unit.get('message'), 'year': str(datetime.now().year)}), _subtype='html'))
                 _server.sendmail(_email.get('login'), _unit.get('to'), _message.as_string())
             _dialogAlert.ui.labelMessage.setText("Почта в очереди была отправлена!")
             _dialogAlert.exec()
@@ -162,7 +154,40 @@ class execute(QtWidgets.QMainWindow):
     def buttonAddUser_clicked(self):
         print("Placeholder for buttonAddUser_clicked()")
     def itemUsers_doubleClicked(self, _x, _y):
-        _usersId = int(self.ui.tableUsers.item(_x, 3).text())
+        _user = DIRECTORY_API.users().get(userKey=int(self.ui.tableUsers.item(_x, 3).text())).execute()
+        _dialogEditUser.ui.editLastName.setText(_user.get('name').get('givenName'))
+        _dialogEditUser.ui.editFirstName.setText(_user.get('name').get('familyName'))
+        _dialogEditUser.ui.editPrimaryEmail.setText(_user.get('primaryEmail'))
+        _dialogEditUser.ui.editOrgUnitPath.setText(_user.get('orgUnitPath'))
+        _subRecEmail = '' if _user.get('recoveryEmail') == None else _user.get('recoveryEmail')
+        _dialogEditUser.ui.editRecoveryEmail.setText(_subRecEmail)
+        _subRecPhone = '' if _user.get('phones') == None else _user.get('phones')[0].get('value')
+        _dialogEditUser.ui.editRecoveryMobilePhone.setText(_subRecPhone)
+        _extId = '' if _user.get('externalIds') == None else _user.get('externalIds')[0].get('value')
+        _dialogEditUser.ui.editEmployeeId.setText(_extId)
+        _addresses = {
+            'work': '',
+            'home': ''
+        }
+        if _user.get('addresses') != None:
+            for _address in _user.get('addresses'):
+                _addresses.update({_address.get('type'): '{}, "{}"'.format(_addresses.get(_address.get('type')), _address.get('formatted'))})
+            _addresses.update({'work': _addresses.get('work')[2:]})
+            _addresses.update({'home': _addresses.get('home')[2:]})
+        _dialogEditUser.ui.editWorkAddress.setText(_addresses.get('work'))
+        _dialogEditUser.ui.editHomeAddress.setText(_addresses.get('home'))
+        if _user.get('suspended'):
+            _dialogEditUser.ui.comboEmployeeStatus.setCurrentIndex(1)
+        elif _user.get('archived'):
+            _dialogEditUser.ui.comboEmployeeStatus.setCurrentIndex(2)
+        else:
+            _dialogEditUser.ui.comboEmployeeStatus.setCurrentIndex(0)
+        if _user.get('changePasswordAtNextLogin'):
+            _dialogEditUser.ui.comboChangePassword.setCurrentIndex(1)
+        else:
+            _dialogEditUser.ui.comboChangePassword.setCurrentIndex(0)
+        _dialogEditUser.exec()
+
 
 # Инициализация формы настройки файла электронной почты
 class dialogEmailSettings(QtWidgets.QDialog):
@@ -248,11 +273,24 @@ class dialogCheckEmail(QtWidgets.QDialog):
             sendMail([{
                 'to': self.ui.editEmail.text(),
                 'subject': 'Проверка подключения к SMTP-серверу',
-                'message': 'Если вы это читаете, то это означает, что все работает корректно!'
+                'message': getFormattedText('email/checkConnect.mail')
             }])
         else:
             _dialogAlert.ui.labelMessage.setText('Для того, чтобы проверить подключение, нужно ввести корректный адрес электронной почты!')
             _dialogAlert.exec()
+
+# Инициализация формы редактирования пользователя
+class dialogEditUser(QtWidgets.QDialog):
+    def __init__(self):
+        super(dialogEditUser, self).__init__()
+        self.ui = Ui_form_editUser()
+        self.ui.setupUi(self)
+    def buttonSave_clicked(self):
+        print("buttonSave_clicked")
+    def buttonCreatePassword_clicked(self):
+        print("buttonCreatePasswrod_clicked")
+    def buttonCreateEmployeeId_clicked(self):
+        print("buttinCreateEmployeeId_clicked")
 
 # Конечная инициализация переменных оконных приложений
 app = QtWidgets.QApplication([])
@@ -262,6 +300,7 @@ _dialogAlert = dialogAlert()
 _dialogJSON = dialogJSONCreds()
 _emailSettings = dialogEmailSettings()
 _checkEmail = dialogCheckEmail()
+_dialogEditUser = dialogEditUser()
 
 # Подпрограмма запуска оконного приложения
 try:
